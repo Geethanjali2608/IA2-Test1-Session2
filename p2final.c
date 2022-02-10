e6c13f9604bc1c9ed683c71d068e831c15176112
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-02-10 09:49:00 +0000

    pro 2

---
+++ p2final.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int input_side()
+{
+  int a;
+  printf("enter value of side:\n");
+  scanf("%d",&a);
+  return a;
+}
+int check_scalene(int a,int b,int c)
+{
+  int isscalene;
+  if(a==b && a==c)
+  {
+   isscalene=1;
+  }
+  else
+  {
+   isscalene=2;
+  }
+  return isscalene;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if(isscalene=1)
+  {
+    printf("given triangle is not scalene\n");
+  }
+  else
+  {
+    printf("given triangle is scalene\n");
+  }
+}
+int main()
+{
+  int a,b,c,isscalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  isscalene=check_scalene(a,b,c);
+  output(a,b,c,isscalene);
+  return 0;
+}
