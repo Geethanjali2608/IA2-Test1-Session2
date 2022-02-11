8eb8d5dbcdb919d2f0eab7457860bf934348700c
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-02-11 09:03:36 +0000

    pro 3

---
+++ p4final.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the no. of times inputs to be taken:\n");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n,int a[n])
+{
+  for(i=)
+}
