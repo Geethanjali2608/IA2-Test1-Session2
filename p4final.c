d595416e5e0034ea38ce247e415d3bb1be9443f8
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-02-12 05:20:09 +0000

    pro 5

--- p4final.c
+++ p4final.c
@@ -8,5 +8,38 @@ int input_array_size()
 }
 void input_array(int n,int a[n])
 {
-  for(i=)
+  for(int i=0;i<n;i++)
+  {
+    printf("enter any value:\n");
+    scanf("%d",&a[n]);
+  }
+}
+int sum_composite_number(int n,int a[n])
+{ 
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+    for(int x=2;x=a[n]/2;x++)
+    {
+      if(a[n]%i==0)
+      {
+        sum=sum+a[n];
+      }  
+    }
+  }
+  return sum;
+}
+void output(int sum)
+{
+  printf("%d",sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  sum=sum_composite_number(n,a);
+  output(sum);
+  return 0;
 }
