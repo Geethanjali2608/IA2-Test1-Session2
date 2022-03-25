c8a2c213dd99fd0be5ad9580acbd64a59226c7a0
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-03-25 16:21:29 +0000

    pro 1(asgnmt)

--- p1final.c
+++ p1final.c
@@ -22,5 +22,26 @@ int main()
   output(base,height,area);
   return 0;
 }
-
   
+/*#include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("enter base and height\n");
+  scanf("%f %f",base,height);
+}
+void find_area(float base,float height,float *area)
+{
+  *area=0.5*base*height;
+}
+void output(float base,float height,float area)
+{
+  printf("the area of the triangle with base %f and height $f is %f\n",base,height,area);
+}
+int main()
+{
+  float base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
+}*/
