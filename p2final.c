6916a6c3f667a2149f3cb576c616b3ddde2ea3e4
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-03-28 16:01:51 +0000

    pro 4

--- p2final.c
+++ p2final.c
@@ -1,6 +1,51 @@
 #include<stdio.h>
 int input_side()
 {
+  int a;
+  printf("enter value of side:\n");
+  scanf("%d",&a);
+  return a;
+}
+int check_scalene(int a,int b,int c)
+{
+  int isscalene;
+  if(a==b && a==c)
+  {
+   isscalene=1;
+  }
+  else
+  {
+   isscalene=2;
+  }
+  return isscalene;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if(isscalene=1)
+  {
+    printf("given triangle is not scalene\n");
+  }
+  else
+  {
+    printf("given triangle is scalene\n");
+  }
+}
+int main()
+{
+  int a,b,c,isscalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  isscalene=check_scalene(a,b,c);
+  output(a,b,c,isscalene);
+  return 0;
+}
+
+
+
+/*#include<stdio.h>
+int input_side()
+{
   int a,b,c;
   printf("enter side value:\n");
   scanf("%d",&a);
@@ -15,7 +60,7 @@ int check_scalene(int a,int b,int c)
   }  
   else
   {
-    isscalene=0;
+    isscalene=2;
   }
   return isscalene;
 }
@@ -39,4 +84,4 @@ int main()
   check_scalene(a,b,c);
   output(a,b,c,isscalene);
   return 0;
-}
+}*/
--- p4final.c
+++ p4final.c
@@ -1,4 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int input_array_size()
+{
+  int k;
+  printf("Enter the length of the array:\n");
+  scanf("%d",&k);
+  return k;
+}
+
+void input_array(int n, int a[n])
+{
+  printf("Enter all values of the array\n");
+  for (int i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+}
+
+int is_composite(int n)
+{
+  for (int i=2;i<=n/2;i++)
+  {
+    if (n%i==0)
+      return 1;
+  }
+  return 0;
+}
+int sum_composite_numbers(int n, int a[n])
+{
+  int sum=0;
+  for (int i=0;i<n;i++)
+  {
+    if (is_composite(a[i]))
+    {
+      sum+=a[i];
+    }
+  }
+  return sum;
+}
+void output(int sum)
+{
+  printf("%d\n",sum);
+}
+int main()
+{
+  int a=input_array_size();
+  int data[a];
+  input_array(a, data);
+  int result=sum_composite_numbers(a, data);
+  output(result);
+  return 0;
+}
+
+
+
+
+/*#include<stdio.h>
 int input_array_size()
 {
   int n;
@@ -42,4 +99,4 @@ int main()
   sum=sum_composite_number(n,a);
   output(sum);
   return 0;
-}
+}*/
